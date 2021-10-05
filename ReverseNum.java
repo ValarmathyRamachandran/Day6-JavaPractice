3f67e35d87aa45cf5fc91e6cbb1750c68570b7db
Valarmathy <valarmathy.ramachandran@outlook.com>
2021-10-05 20:22:52 +0530

    Reveresed the given num

---
+++ ReverseNum.java
@@ -0,0 +1,29 @@
+package ReverseNum;
+import java.util.Scanner;
+class ReverseNum {
+		  public static void main(String[] args) {
+
+		    Scanner scan=new Scanner(System.in);
+			int num=scan.nextInt();
+			 scan.close();
+				System.out.println("Please Enter the Number:");
+
+		    int reversed=0;
+			// run loop until num becomes 0
+		    while(num != 0) {
+		    
+		      // get last digit from num
+		      int digit = num % 10;
+		      reversed = reversed * 10 + digit;
+
+		      // remove the last digit from num
+		      num = num / 10;
+		    }
+
+		    System.out.println("Reversed Number: " + reversed);
+		  
+		  }
+		}
+
+
+
